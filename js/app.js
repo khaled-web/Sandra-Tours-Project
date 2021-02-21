06a4ecaec8fe421285bd409d728ad8405bcc3f34
khaled-web <eng.khaled_ok@outlook.com>
2021-02-21 17:36:30 +0200

    Full Package For Sandra Tours Project

--- js/app.js
+++ js/app.js
@@ -11,35 +11,35 @@ navBtn.addEventListener("click", () => {
  links.classList.toggle("show-links");
 });
 
-// // ********** nav toggle ************
-// const clearField = () => {
-//  let field, fieldArr;
-//  field = document.querySelectorAll('.form-control');
-//  fieldArr = Array.from(field);
-//  fieldArr.forEach((el) => el.value = '');
-// }
+// ********** nav toggle ************
+const clearField = () => {
+ let field, fieldArr;
+ field = document.querySelectorAll('.form-control');
+ fieldArr = Array.from(field);
+ fieldArr.forEach((el) => el.value = '');
+}
 
 document.querySelector('.btn-submit').addEventListener('click', clearField);
 
-// // ********** smooth scroll ************
-// // select links
-// const scrollLinks = document.querySelectorAll(".scroll-link");
-// scrollLinks.forEach(link => {
-//   link.addEventListener("click", e => {
-//     // prevent default
-//     e.preventDefault();
-//     links.classList.remove("show-links");
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+ link.addEventListener("click", e => {
+  // prevent default
+  e.preventDefault();
+  links.classList.remove("show-links");
 
-//     const id = e.target.getAttribute("href").slice(1);
-//     const element = document.getElementById(id);
-//     //
-//     let position = element.offsetTop - 62;
+  const id = e.target.getAttribute("href").slice(1);
+  const element = document.getElementById(id);
+  //
+  let position = element.offsetTop - 62;
 
-//     window.scrollTo({
-//       left: 0,
-//       // top: element.offsetTop,
-//       top: position,
-//       behavior: "smooth"
-//     });
-//   });
-// });
+  window.scrollTo({
+   left: 0,
+   // top: element.offsetTop,
+   top: position,
+   behavior: "smooth"
+  });
+ });
+});
